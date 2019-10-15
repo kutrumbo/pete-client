528c41ee32bb882f4ae2891977cfd54c1b34ade4
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2019-10-14 23:53:01 -0700

    Toggle active status in activity list

--- screens/ActivityScreen.js
+++ screens/ActivityScreen.js
@@ -1,53 +1,62 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { FlatList, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+import { map } from 'lodash';
 
 import Colors from '../constants/Colors';
 import Layout from '../constants/Layout';
 
-const DATA = [
+const INITIAL_DATA = [
   {
     id: 'mindfulness',
     title: 'Mindfulness',
+    active: false,
   },
   {
     id: 'running',
     title: 'Running',
+    active: false,
   },
   {
     id: 'sport',
     title: 'Sport',
+    active: false,
   },
   {
     id: 'surfing',
     title: 'Surfing',
+    active: false,
   },
   {
     id: 'yoga',
     title: 'Yoga',
+    active: false,
   },
 ];
 
-function onPress() {
-  console.log('hi');
-}
-
-function Item({ active, title }) {
+function Item({ active, dispatch, id, title }) {
   return (
     <TouchableHighlight
       style={[styles.item, active && styles.activeItem]}
-      onPress={onPress}
+      onPress={() => dispatch({ id })}
       underlayColor={Colors.lightTintColor}>
       <Text style={styles.itemLabel}>{title}</Text>
     </TouchableHighlight>
   );
 }
 
+function reducer(state, action) {
+  return map(state, item => (item.id === action.id ? { ...item, active: !item.active } : item));
+}
+
 export default function ActivityScreen() {
+  const [state, dispatch] = useReducer(reducer, INITIAL_DATA);
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={DATA}
-        renderItem={({ item }) => <Item {...item} />}
+        data={state}
+        renderItem={({ item }) => <Item {...item} dispatch={dispatch} />}
         keyExtractor={item => item.id}
       />
     </View>
